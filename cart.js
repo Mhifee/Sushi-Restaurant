737e9101f3720fc0f5b8f1d4ebbcc639a76ac054
Mhifee <dollypkemi@gmail.com>
2022-10-28 13:28:55 +0100

    sushi-restaurant

---
+++ cart.js
@@ -0,0 +1,27 @@
+function increaseCount (a, b) {
+    var input = b.previousElementSibling;
+    var value = parseInt (input.value, 10);
+    value = isNaN (value)? 0 : value;
+    value ++;
+    input.value = value;
+}
+function decreaseCount (a, b) {
+    var input = b.nextElementSibling;
+    var value = parseInt (input.value, 10);
+    if (value > 1) {
+        value = isNaN (value)? 0 : value;
+        value --;
+        input.value = value;
+    }
+}
+
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+}
+
+function clearInput(){
+    var getValue= document.getElementById("cancel");
+      if (getValue.value !="") {
+          getValue.value = "";
+      }
+}
